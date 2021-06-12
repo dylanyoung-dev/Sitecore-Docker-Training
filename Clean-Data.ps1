95f1b6e85f8959ca3aac2083499487a6e870078e
sitecoremaster <dylan.young@igniumdigital.com>
2021-06-12 16:34:33 -0400

    CleanData folder fixes

--- Clean-Data.ps1
+++ Clean-Data.ps1
@@ -3,3 +3,9 @@ Get-ChildItem -Path (Join-Path $PSScriptRoot "docker\data") -Directory | ForEach
 
     Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
 }
+
+Get-ChildItem -Path (Join-Path $PSScriptRoot "docker\deploy\platform") -Directory | ForEach-Object {
+    $dataPath = $_.FullName
+
+    Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+}
